24698c5ba81b8b2aef2a50f91ee400b01e8e40f3
L1ghtn1ng1 <77595914+L1ghtn1ng1@users.noreply.github.com>
2025-01-16 16:05:39 +1300

    configuring done

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+// webpack.config.js
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/script.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/index.html"],
+    },
+  plugins : [
+    new HTMLWebpackPlugin({
+        template: "./src/index.html"
+        }) 
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            }
+        ]
+    }
+};
